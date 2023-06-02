891e8cd1ef6cde9163bbfcc729936067c4dfbef0
Zeeshan-jamal <zeeshan.jamal@northsouth.edu>
2023-06-02 21:20:23 +0600

    6th commit

---
+++ project_8_Linq_generic/quiz_problem/Program.cs
@@ -0,0 +1,86 @@
+﻿
+//1.which of the following keyword is used for including the namespaces in in the program in C#? A. Import B. Using
+
+//2.which of the following converts a type to a string in C#? A. ToString B. ToInt32
+
+//3.which of the following operator represents a conditional operation in C#? A. = B. ?
+
+//4. What is a correct syntax to output "Hello world" in C#? A. Console B. Print
+
+//5. C# is an aias of C++ A. True B. False
+
+
+using System.Collections;
+
+//string[] q =
+//{
+//    "1.which of the following keyword is used for including the namespaces in in the program in C#? A. Import B. Using",
+//    "2.which of the following converts a type to a string in C#? A. ToString B. ToInt32",
+//    "3.which of the following operator represents a conditional operation in C#? A. = B. ?" ,
+//    "4. What is a correct syntax to output 'Hello world' in C#? A. Console B. Print" ,
+//    "5. C# is an aias of C++ A. True B. False"
+//};
+
+
+
+//foreach (string s in q)
+//{
+//    Console.WriteLine(s);
+
+//}
+
+
+namespace quiz_problem
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Q_A> list = new List<Q_A>()
+            {
+                new Q_A{questions="1.which of the following keyword is used for including the namespaces in in the program in C#? A. Import B. Using",answers="B" },
+                new Q_A{questions="2.which of the following converts a type to a string in C#? A. ToString B. ToInt32",answers="A" },
+                new Q_A{questions="3.which of the following operator represents a conditional operation in C#? A. = B. ?",answers="A" },
+                new Q_A{questions= "4. What is a correct syntax to output 'Hello world' in C#? A. Console.Writeline() B. Print",answers="A" },
+                new Q_A{questions="5. C# is an aias of C++ A. True B. False",answers="A" }
+            };
+
+            //string[] yourans = new string[100];
+            int point = 0;
+
+
+            //yourans[i] = Console.ReadLine();
+            foreach (Q_A q in list)
+            {
+                Console.WriteLine(q.questions);
+                q.yourans = Console.ReadLine();
+
+                if (q.yourans == q.answers)
+                {
+                    point += 10;
+                }
+                else
+                {
+                    point += 0;
+                }
+            }
+            foreach (Q_A q in list)
+            {
+                Console.WriteLine(q.questions+" "+q.yourans+" "+q.answers); 
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ project_8_Linq_generic/quiz_problem/Q_A.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quiz_problem
+{
+    public class Q_A
+    {
+        public string questions { get; set; }
+        public string answers { get; set; }
+        public string yourans{ get; set; }
+}
+}
